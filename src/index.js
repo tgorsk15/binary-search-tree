bfd062279d5ca078d04e3da4c3d1984d37b35304
tgorsk15 <tgbusiness463@gmail.com>
2024-03-05 18:47:34 -0600

    Modify driver script
    Script is located in index.js file

--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
 import { treeFactory } from "./binary-tree";
 
+// driver script:
 
-const testArray = [8, 6, 9, 9, 9, 10, 31, 45, 22, 23, 2, 14];
+const testArray = [8, 6, 9, 9, 9, 10, 12, 13, 31, 45, 22, 23, 2, 14];
 const testRun = treeFactory(testArray);
 
 
-testRun.deleteNode(6);
+// testRun.deleteNode(6);
 
 testRun.insert(7);
 testRun.insert(4);
 testRun.insert(5);
 testRun.insert(3);
-testRun.insert(6);
 
+// testRun.find(22);
 
-testRun.find(22);
+testRun.isBalanced();  // logs "right side too short"
 
-// testRun.levelOrder()
-
+testRun.levelOrder()
+testRun.preOrder();
+testRun.inOrder();
+testRun.postOrder();
 // testRun.height();
-// testRun.depth(16);
 
+testRun.rebalance();
 testRun.isBalanced();
 
+// testRun.levelOrder()
 // testRun.preOrder();
 // testRun.inOrder();
 // testRun.postOrder();
-testRun.rebalance();
 
-testRun.insert(52);
-testRun.isBalanced();
 
-// testRun.insert(60);
-// testRun.isBalanced()
